6399ab7a69ec6bb4ee76e45c7e23a3eb9ecc4eb8
natthapach <natthapach.a@ku.th>
2017-09-25 15:14:09 +0700

    implement SQLite manager

--- Customer/src/main/java/CustomerApplication.java
+++ Customer/src/main/java/CustomerApplication.java
@@ -1,7 +1,20 @@
+import controllers.CoreController;
+import controllers.MainController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import views.RootView;
+
+import java.io.IOException;
 
 public class CustomerApplication extends Application {
+
+
+    private CoreController controller;
+    private Stage primaryStage;
+    private RootView rootView;
 
     public static void main(String[] args) {
         launch(args);
@@ -9,6 +22,26 @@ public class CustomerApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        this.controller = new MainController();
+        this.controller.start();
+        this.primaryStage = primaryStage;
+        initRoot();
+    }
 
+    private void initRoot() {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/MainView.fxml"));
+            Pane mainLayout = loader.load();
+            rootView = loader.getController();
+            rootView.setController(controller);
+
+            Scene sc = new Scene(mainLayout);
+            primaryStage.setScene(sc);
+            primaryStage.show();
+            primaryStage.setTitle("Little Bear Buffet");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Customer/src/main/java/views/MainViewCustomer.java
+++ Customer/src/main/java/views/MainViewCustomer.java
@@ -1,6 +1,20 @@
 package views;
 
 
-public class MainViewCustomer {
+import controllers.CoreController;
+import models.Order;
 
+public class MainViewCustomer implements RootView {
+
+    public void setController(CoreController controller) {
+
+    }
+
+    public void addOrder(Order order) {
+
+    }
+
+    public void checkBill() {
+
+    }
 }
