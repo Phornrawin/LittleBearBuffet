3b81e31ab0386dd5c80b35fab89140f78825076e
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-09-26 12:40:56 +0700

    implement UrlStorage class continue

--- Customer/src/main/java/models/UrlStorage.java
+++ Customer/src/main/java/models/UrlStorage.java
@@ -17,5 +17,21 @@ public class UrlStorage {
         imageGrilledUrl.put("Chicken", "https://goo.gl/bFSXRz");
         imageGrilledUrl.put("shrimp", "https://goo.gl/kSF7m4");
         imageGrilledUrl.put("cowTongue", "https://goo.gl/MQq3tE");
+        imageGrilledUrl.put("Egg", "https://goo.gl/fVt6RN");
+        imageGrilledUrl.put("EnokiMashroom", "https://goo.gl/vSbaq6");
+        imageGrilledUrl.put("EringiMashroom", "https://goo.gl/kQPvYN");
+        imageGrilledUrl.put("FishNoodle", "https://goo.gl/Mqq1Fr");
+        imageGrilledUrl.put("Guangdong", "https://goo.gl/FzRW3A");
+        imageGrilledUrl.put("HeartPork", "https://goo.gl/WQ4ELx");
+        imageGrilledUrl.put("JapaneseVermicelli", "https://goo.gl/G7daJJ");
+        imageGrilledUrl.put("JellyFish", "https://goo.gl/uNiU1i");
+        imageGrilledUrl.put("Kamakobo", "https://goo.gl/7wuKAm");
+        imageGrilledUrl.put("LeanBeef", "https://goo.gl/T5vRzc");
+        imageGrilledUrl.put("MahiMahiFish", "https://goo.gl/YDB2sH");
+        imageGrilledUrl.put("Narutomaki", "https://goo.gl/wmXiP3");
+        imageGrilledUrl.put("PorkSlide", "https://goo.gl/LCRdbM");
+        imageGrilledUrl.put("PorkLiver", "https://goo.gl/3jedio");
+        imageGrilledUrl.put("punpkin", "https://goo.gl/c2Gguj");
+
     }
 }
