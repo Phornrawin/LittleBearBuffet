dee4153f218d1a1be48d73ea3696684280bed389
natthapach <natthapach.a@ku.th>
2017-10-08 19:47:11 +0700

    implement modelObservable on Item

--- API/src/main/java/models/Item.java
+++ API/src/main/java/models/Item.java
@@ -1,10 +1,14 @@
 package models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Item implements ModelObserable<Item> {
     private int id;
     private String name;
     private int categoryId;
     private int balance;
+    private Set<ModelObserver<Item>> observers;
 
 
     public Item(int id, String name, int categoryId, int balance){
@@ -12,6 +16,7 @@ public class Item implements ModelObserable<Item> {
         this.name = name;
         this.categoryId = categoryId;
         this.balance = balance;
+        observers = new HashSet<ModelObserver<Item>>();
     }
     public Item(int id, String name, int categoryId) {
         this(id, name, categoryId, 0);
@@ -31,6 +36,7 @@ public class Item implements ModelObserable<Item> {
 
     public void setBalance(int balance){
         this.balance = balance;
+        notifyObsrvers();
     }
     public int getBalance(){
         return balance;
@@ -46,10 +52,15 @@ public class Item implements ModelObserable<Item> {
     }
 
     public void regisObserver(ModelObserver<Item> observer) {
-
+        observers.add(observer);
     }
 
     public void removeObserver(ModelObserver<Item> observer) {
+        observers.remove(observer);
+    }
 
+    private void notifyObsrvers(){
+        for (ModelObserver<Item> observer : observers)
+            observer.onModelChange(this);
     }
 }
