5ea415079fc04d9c361788ef51e8be9a1308545f
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-11-24 11:30:02 +0700

    Cashier: create Module 2

---
+++ Cashier/src/main/java/views/Mainview.java
@@ -0,0 +1,12 @@
+package views;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Mainview implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
