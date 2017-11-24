84a80f090670ba134f1477b4d858866b26b9b4c7
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-11-24 12:03:01 +0700

    Cashier: implement CashierApplication

--- Cashier/src/main/java/controllers/MainController.java
+++ Cashier/src/main/java/controllers/MainController.java
@@ -1,4 +1,21 @@
 package controllers;
 
-public class MainController {
+import models.Payment;
+import views.CashierView;
+
+import java.util.List;
+
+public class MainController implements PaymentController{
+
+    public void pay(Payment payment) {
+
+    }
+
+    public List<Payment> getPayments() {
+        return null;
+    }
+
+    public void setView(CashierView view) {
+
+    }
 }
