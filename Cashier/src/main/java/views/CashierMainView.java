3793a4dffb677fe5359eede47991b7b0c0e0d183
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-11-24 13:14:27 +0700

    Cashier: implement CashierMainView

--- Cashier/src/main/java/views/CashierMainView.java
+++ Cashier/src/main/java/views/CashierMainView.java
@@ -1,7 +1,11 @@
 package views;
 
 import controllers.PaymentController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import models.Payment;
 
 import java.net.URL;
@@ -9,7 +13,10 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class CashierMainView implements Initializable, CashierView {
-    private List<Payment> payments;
+    @FXML private ComboBox cb_table;
+    @FXML private Label lb_peopleAmt, lb_totalPrice, lb_eachPrice;
+    @FXML private Button btn_delete, btn_dot, btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9;
+    private List<Payment> payments; 
     private PaymentController controller;
 
     public void initialize(URL location, ResourceBundle resources) {
@@ -17,7 +24,7 @@ public class CashierMainView implements Initializable, CashierView {
     }
 
     public void setAvailable(List<Payment> payments) {
-
+        this.payments = payments;
     }
 
     public void onPayComplete(Payment payment) {
